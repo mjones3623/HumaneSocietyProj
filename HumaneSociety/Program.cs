33c2decf8cb78cea693186a20c68010d12eaf45a
Jonathan Friedl <jfreetz2@gmail.com>
2020-03-10 15:58:11 -0500

    Got the remove method done but not sure how to test it started the update animal method-Jonathan

--- HumaneSociety/Program.cs
+++ HumaneSociety/Program.cs
@@ -11,6 +11,8 @@ namespace HumaneSociety
         static void Main(string[] args)
         {
             PointOfEntry.Run();
+            //Animal animal = new Animal();
+            //Query.AddAnimal(animal);
         }
     }
 }
