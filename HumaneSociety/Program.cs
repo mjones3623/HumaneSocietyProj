ec4fdf4d204c4e169fd48522f779dc547ba917f3
Mathew Jones <anilox123@gmail.com>
2020-03-11 15:49:13 -0500

    resolved merge conflict

--- HumaneSociety/Program.cs
+++ HumaneSociety/Program.cs
@@ -10,18 +10,10 @@ namespace HumaneSociety
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            //Admin admin = new Admin();
 
-            //admin.LogIn();
 
             //PointOfEntry.Run();
-=======
-            //PointOfEntry.Run();
-            HumaneSocietyDataContext db = new HumaneSocietyDataContext();
-            Employee employee = db.Employees.FirstOrDefault(a => a.EmployeeId == 1);
-            Query.RunEmployeeQueries(employee, "read");
->>>>>>> 33947ef95e6ce33aa7c366fe8e534fa716510b79
+
         }
     }
 }
